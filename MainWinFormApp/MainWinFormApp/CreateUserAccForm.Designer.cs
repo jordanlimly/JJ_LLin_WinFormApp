92c2b49f0fd0619244e7981ba56183ba8e284dce
itslideon <lideonleon15@gmail.com>
2021-11-24 17:14:34 +0800

    create account form

---
+++ MainWinFormApp/MainWinFormApp/CreateUserAccForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace MainWinFormApp
+{
+    partial class frmCreateuserAcc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCreateUserAcc = new System.Windows.Forms.Label();
+            this.lblFullname = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.lblCardID = new System.Windows.Forms.Label();
+            this.tbFullName = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbCfmPassword = new System.Windows.Forms.TextBox();
+            this.tbCardID = new System.Windows.Forms.TextBox();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCreateUserAcc
+            // 
+            this.lblCreateUserAcc.AutoSize = true;
+            this.lblCreateUserAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCreateUserAcc.Location = new System.Drawing.Point(273, 29);
+            this.lblCreateUserAcc.Name = "lblCreateUserAcc";
+            this.lblCreateUserAcc.Size = new System.Drawing.Size(297, 46);
+            this.lblCreateUserAcc.TabIndex = 0;
+            this.lblCreateUserAcc.Text = "Create Account";
+            this.lblCreateUserAcc.Click += new System.EventHandler(this.lblCreateUserAcc_Click);
+            // 
+            // lblFullname
+            // 
+            this.lblFullname.AutoSize = true;
+            this.lblFullname.Location = new System.Drawing.Point(189, 134);
+            this.lblFullname.Name = "lblFullname";
+            this.lblFullname.Size = new System.Drawing.Size(79, 17);
+            this.lblFullname.TabIndex = 1;
+            this.lblFullname.Text = "Full Name: ";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(189, 178);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(50, 17);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Email: ";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(189, 227);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(77, 17);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password: ";
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Location = new System.Drawing.Point(189, 272);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(129, 17);
+            this.lblConfirmPassword.TabIndex = 4;
+            this.lblConfirmPassword.Text = "Confirm Password: ";
+            // 
+            // lblCardID
+            // 
+            this.lblCardID.AutoSize = true;
+            this.lblCardID.Location = new System.Drawing.Point(189, 322);
+            this.lblCardID.Name = "lblCardID";
+            this.lblCardID.Size = new System.Drawing.Size(63, 17);
+            this.lblCardID.TabIndex = 5;
+            this.lblCardID.Text = "Card ID: ";
+            // 
+            // tbFullName
+            // 
+            this.tbFullName.Location = new System.Drawing.Point(321, 129);
+            this.tbFullName.Name = "tbFullName";
+            this.tbFullName.Size = new System.Drawing.Size(290, 22);
+            this.tbFullName.TabIndex = 6;
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(321, 173);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(290, 22);
+            this.tbEmail.TabIndex = 7;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(321, 224);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(290, 22);
+            this.tbPassword.TabIndex = 8;
+            // 
+            // tbCfmPassword
+            // 
+            this.tbCfmPassword.Location = new System.Drawing.Point(321, 269);
+            this.tbCfmPassword.Name = "tbCfmPassword";
+            this.tbCfmPassword.Size = new System.Drawing.Size(290, 22);
+            this.tbCfmPassword.TabIndex = 9;
+            // 
+            // tbCardID
+            // 
+            this.tbCardID.Location = new System.Drawing.Point(321, 322);
+            this.tbCardID.Name = "tbCardID";
+            this.tbCardID.Size = new System.Drawing.Size(290, 22);
+            this.tbCardID.TabIndex = 10;
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Location = new System.Drawing.Point(192, 381);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(160, 49);
+            this.btnSignUp.TabIndex = 11;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(591, 381);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(160, 49);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // frmCreateuserAcc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 473);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.tbCardID);
+            this.Controls.Add(this.tbCfmPassword);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbFullName);
+            this.Controls.Add(this.lblCardID);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblFullname);
+            this.Controls.Add(this.lblCreateUserAcc);
+            this.Name = "frmCreateuserAcc";
+            this.Text = "CreateUserAccForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCreateUserAcc;
+        private System.Windows.Forms.Label lblFullname;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.Label lblCardID;
+        private System.Windows.Forms.TextBox tbFullName;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.TextBox tbCfmPassword;
+        private System.Windows.Forms.TextBox tbCardID;
+        private System.Windows.Forms.Button btnSignUp;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ MainWinFormApp/MainWinFormApp/CreateUserAccForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainWinFormApp
+{
+    public partial class frmCreateuserAcc : Form
+    {
+        public frmCreateuserAcc()
+        {
+            InitializeComponent();
+        }
+
+        // i accidentally clicked the title im so sworryy :(((((
+        private void lblCreateUserAcc_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
